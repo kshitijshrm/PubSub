bc8dee49b3d02783999dee9325402ec92cf9e41c
kshitijsharmadelhivery <kshitij.sharma@delhivery.com>
2025-05-25 00:22:58 +0530

    Removed env variable from test environment

--- receiver-service/.jest/setEnvVars.ts
+++ receiver-service/.jest/setEnvVars.ts
@@ -1,5 +1,5 @@
-process.env.MONGO_URI = "mongodb+srv://kshitijshrm:B3c82PZaFsipsRL9@cluster0.k74biel.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-process.env.RECEIVER_PORT = "3000"
-process.env.LISTENER_PORT = "4000"
-process.env.REDIS_HOST = "localhost"
-process.env.REDIS_PORT = "6379"
+process.env.MONGO_URI = ""// please pass mongo uri here;
+process.env.RECEIVER_PORT = ""
+process.env.LISTENER_PORT = ""
+process.env.REDIS_HOST = ""
+process.env.REDIS_PORT = ""
